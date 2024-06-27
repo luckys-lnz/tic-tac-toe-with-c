64f741fba2c907f6da28701bd0008248c007d672
luckys-lnz <lalucky22@gmail.com>
2024-06-27 14:29:19 +0000

    function declarations

---
+++ tic-tac-toe.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+/* A tic-tac-toe-game */
+
+const char PLAYER = 'O';
+const char COMPUTER = 'X';
+
+void resetBoard();
+void printBoard();
+int checkFreeSpace();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner();
+
+int main()
+{
+    return 0;
+}
+
+void resetBoard();
+void printBoard();
+int checkFreeSpace();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner();
